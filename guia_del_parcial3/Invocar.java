fad18f4c84bc3ae05a9620f91f7d50fbd3f2332c
ChristianDeras <71231223+ChristianDeras@users.noreply.github.com>
2020-10-16 11:11:55 -0600

    CREACION DE INVOCAR

--- guia_del_parcial3/Invocar.java
+++ guia_del_parcial3/Invocar.java
@@ -1,5 +1,29 @@
 package guia_del_parcial3;
-
+import java.util.*;
 public class Invocar {
-    
+    public void Invoca(){
+        int resp=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("INVOCANDO TODAS LAS CLASES ANTERIORES ");
+        System.out.println("INGRESE LA CLASE QUE DESEA INVOCAR");
+        System.out.println("1.Estructuras de control  2.Alumnnos  3.Evaluaciones");
+        while (resp !=1 && resp !=2 && resp !=3) {
+            resp= sc.nextInt();
+            switch (resp) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                 case 3:
+                    
+                    break;
+                default:
+                System.out.println("Datos incorrectos por favor intenta de nuevo");
+                resp--;
+                    break;
+            }
+        }
+    }
 }
--- principal.java
+++ principal.java
@@ -1,5 +1,7 @@
+import guia_del_parcial3.*;
 class principal{
     public static void main(String[] args) {
-        
+        Invocar i1 = new Invocar();
+        i1.Invoca();
     }
 }
